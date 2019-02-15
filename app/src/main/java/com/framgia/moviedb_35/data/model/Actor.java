9a981b7a32fe58aeb049d61aea70f13e2469cc63
QUANGTRAN <dinhquang251@gmail.com>
2019-02-15 14:07:39 +0700

    6119_Movie_Data

---
+++ app/src/main/java/com/framgia/moviedb_35/data/model/Actor.java
@@ -0,0 +1,75 @@
+package com.framgia.moviedb_35.data.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Actor {
+    @SerializedName("cast_id")
+    @Expose
+    private int mCastId;
+    @SerializedName("credit_id")
+    @Expose
+    private String mCreditId;
+    @SerializedName("id")
+    @Expose
+    private int mId;
+    @SerializedName("name")
+    @Expose
+    private String mName;
+    @SerializedName("profile_path")
+    @Expose
+    private String mProfilePath;
+
+    public Actor(int castId, String creditId, int id, String name, String profilePath) {
+        mCastId = castId;
+        mCreditId = creditId;
+        mId = id;
+        mName = name;
+        mProfilePath = profilePath;
+    }
+
+    public int getCastId() {
+        return mCastId;
+    }
+
+    public Actor setCastId(int castId) {
+        mCastId = castId;
+        return this;
+    }
+
+    public String getCreditId() {
+        return mCreditId;
+    }
+
+    public Actor setCreditId(String creditId) {
+        mCreditId = creditId;
+        return this;
+    }
+
+    public int getId() {
+        return mId;
+    }
+
+    public Actor setId(int id) {
+        mId = id;
+        return this;
+    }
+
+    public String getName() {
+        return mName;
+    }
+
+    public Actor setName(String name) {
+        mName = name;
+        return this;
+    }
+
+    public String getProfilePath() {
+        return mProfilePath;
+    }
+
+    public Actor setProfilePath(String profilePath) {
+        mProfilePath = profilePath;
+        return this;
+    }
+}
--- app/src/main/java/com/framgia/moviedb_35/screen/detail/DetailViewModel.java
+++ app/src/main/java/com/framgia/moviedb_35/screen/detail/DetailViewModel.java
@@ -4,6 +4,7 @@ import android.databinding.ObservableField;
 
 import com.framgia.moviedb_35.data.model.Movie;
 import com.framgia.moviedb_35.data.repository.MovieRepository;
+import com.framgia.moviedb_35.util.StringUtils;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
 import io.reactivex.disposables.CompositeDisposable;
@@ -13,7 +14,15 @@ import io.reactivex.schedulers.Schedulers;
 
 public class DetailViewModel {
     private static final String APPEND_TO_MOVIE_DETAIL = "videos,credits";
+    private static final String MOVIE_RELEASE = " -release";
+    private static final String MOVIE_MINUTES = " -minutes";
+    private static final String COMMA = " ,";
+    private static final int NUMBER_ONE = 1;
+    private String nameGenres = "";
     public final ObservableField<Movie> movieObservable = new ObservableField<>();
+    public final ObservableField<String> mObservableGenres = new ObservableField<>();
+    public final ObservableField<String> mObservableRunTime = new ObservableField<>();
+    public final ObservableField<String> mObservableRelease = new ObservableField<>();
     private CompositeDisposable mCompositeDisposable = new CompositeDisposable();
     private MovieRepository mMovieRepository;
 
@@ -30,6 +39,7 @@ public class DetailViewModel {
                     @Override
                     public void accept(Movie movie) {
                         movieObservable.set(movie);
+                        setMovieObservable(movie);
                     }
                 }, new Consumer<Throwable>() {
                     @Override
@@ -37,5 +47,19 @@ public class DetailViewModel {
                     }
                 });
         mCompositeDisposable.add(disposable);
+    }
+
+    private void setMovieObservable(Movie movie) {
+        int size = movie.getGenres().size();
+        for (int i = 0; i < size; i++) {
+            if (i == size - NUMBER_ONE) {
+                nameGenres = StringUtils.append(nameGenres, movie.getGenres().get(i).getName());
+            } else {
+                nameGenres = StringUtils.append(nameGenres, movie.getGenres().get(i).getName(), COMMA);
+            }
+        }
+        mObservableGenres.set(nameGenres);
+        mObservableRunTime.set(StringUtils.append(String.valueOf(movie.getRuntime()),MOVIE_MINUTES));
+        mObservableRelease.set(StringUtils.append(movie.getReleaseDate(),MOVIE_RELEASE));
     }
 }
